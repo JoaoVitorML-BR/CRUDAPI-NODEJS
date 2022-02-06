031dda9a6e25b9a1785bd4039ad5551bf5612c8c
JoaoVitorML-BR <jvmlira33@gmail.com>
2022-02-05 22:11:12 -0300

    Meu primeiro CRUD de uma API com NODE.JS

---
+++ README.md
@@ -0,0 +1,15 @@
+# MEU PRIMEIRO CRUD-API COM NODE.JS
+
+- esse é a minha primeira API com Node.js onde ela faz o famoso CRUD.
+- Essa API eu não fiz só, entretanto, irei modificar algumas coisas nela aos poucos, no momento apenas fiz de acordo com o Video da Rocketseat no Youtube.
+- Link: https://www.youtube.com/watch?v=fm4_EuCsQwg&list=LL&index=2&t=2948s
+- no Link acima você poderá fazer o mesmo, ficando assim de melhor entendimento o que cada linha faz, e é claro como preparar o ambiente, pois antes você precisará instalar por exemplo o NODE.JS e o npm.
+
+## Bastante aprendizado
+
+- Acompanhar a professora escrevendo esse código e replicando do lado de cá foi bastante satisfatorio, liberou dopamina no meu cérebro e me fez despertar ideias. inclusive achei mais legal de mexer com web com o Node.js pelo pouco que vi do que com o DJANGO (sem querer desmerecer meu querido e amado Python rsrsrs)
+
+## Sobre a API
+
+- Bom, não tem muito o que se dizer, afinal é um CRUD. um fato interessante foi que salvamos os dados em arquivo JSON, ele escreve, altera e deleta os dados de acordo com o que você faz.
+- Ele também salva o arquivo fazendo com que mesmo que você pare o servidor http, ele após reiniciado volte ao estado com os dados da ultima forma salva. simulando assim um Banco de dados.
---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const {randomUUID} = require("crypto");
+const { response } = require("express");
+const res = require("express/lib/response"); 
+const fs = require("fs"); // fs = file system
+
+const app = express();
+
+app.use(express.json());
+
+let products = [];
+
+fs.readFile("products.json", "utf-8", (err, data) => {
+    if(err){
+        console.log(err);
+    }else{
+        products = JSON.parse(data);
+    }
+});
+
+// cadastrando os produtos 
+app.post("/products", (req, res) => {
+
+    const {name, price}  = req.body; // that name and price in this way is destructuring the json
+
+    const product = {
+        name,
+        price,
+        id: randomUUID(),
+    };
+
+    products.push(product);
+
+    productFile(); // escrevendo o arquivo json \/
+
+    return res.json(product);
+});
+
+
+// rota para listar todos os produtos
+app.get("/products", (req, res) => {
+    return res.json(products)
+});
+
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params; // desestrutura o req para pegar o id
+    const product = products.find(product => product.id === id); // percorre o array e quando achar um produto dentro do array que seja igual ao id da rota ele ira pegar ele.
+    return res.json(product);
+})
+
+
+// aqui ele altera o produto
+app.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const {name, price} = req.body;
+
+    const productIndex = products.findIndex(product => product.id === id);
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price
+    };
+
+    productFile();
+
+    return res.json({message: "Produto alterado com sucesso!"})
+});
+
+// deleta produto
+app.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const {name, price} = req.body
+
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex, 1) // removendo o produto
+
+    productFile();
+
+    return res.json({message: "Produto removido com sucesso."});
+});
+
+
+// func cria a pasta products.json, converte a products em um json e adiciona os "products" na pasta, se der algum erro ele ira cair no if, se não no else.
+function productFile(){
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if (err){
+            console.log(err);
+        }else{
+            console.log("Produto inserido!")
+        }
+    });
+}
+
+app.listen(4002, () => console.log("servidor está rodando na porta 4002"))
